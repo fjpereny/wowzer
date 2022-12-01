286bbcc19f7b6bce88cadb3d2804e10fb2dcb468
Frank Pereny <fjpereny@gmai.com>
2022-12-01 19:19:30 +0800

    Basic screenshot thread created.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,37 @@
 
+import cv2
+from PIL import Image
+import time
+from threading import Thread
+import wowzer
+from Xlib import display, X
+
+
+class MainAgent:
+    def __init__(self):
+        self.cur_img = None
+
+
+def update_screen(agent):
+    print("Starting computer vision screen update...")
+    dsp = display.Display()
+    screen = dsp.screen()
+    root = screen.root
+    width = screen.width_in_pixels
+    height = screen.height_in_pixels
+    print("Detected display resolution: " + str(width) + " x " + str(height))
+    while True:
+        raw = root.get_image(0, 0, width, height, X.ZPixmap, 0xffffff)
+        agent.cur_img = Image.frombytes("RGB", (width, height), raw.data, "raw", "BGRX")
+        print("Screen updated")
+        time.sleep(1)
+
+
 def run():
-    print('main.py')
+    main_agent = MainAgent()
+    update_screen_thread = Thread(target=update_screen, args=(main_agent,), name="update screen thread")
+    update_screen_thread.start()
+
+
+if __name__ == '__main__':
+    wowzer.run()
--- src/wowzer.py
+++ src/wowzer.py
@@ -5,20 +5,20 @@ import main
 APP_NAME = 'wowzer'
 APP_VERSION = '0.0.1'
 AUTHOR = 'Frank Pereny'
-COPRIGHT_YEAR = '2022'
+COPYRIGHT_YEAR = '2022'
 WEBSITE_LINK = 'https://github.com/fjpereny/wowzer'
 
-if __name__ == "__main__":
 
-    header_art = "\n██╗    ██╗ ██████╗ ██╗    ██╗███████╗███████╗██████╗\n"
-    header_art += "██║    ██║██╔═══██╗██║    ██║╚══███╔╝██╔════╝██╔══██╗\n"
-    header_art += "██║ █╗ ██║██║   ██║██║ █╗ ██║  ███╔╝ █████╗  ██████╔╝\n"
-    header_art += "██║███╗██║██║   ██║██║███╗██║ ███╔╝  ██╔══╝  ██╔══██╗\n"
-    header_art += "╚███╔███╔╝╚██████╔╝╚███╔███╔╝███████╗███████╗██║  ██║\n"
-    header_art += "╚══╝╚══╝  ╚═════╝  ╚══╝╚══╝ ╚══════╝╚══════╝╚═╝  ╚═╝\n"
+def run():
+    header_art = "\n\t██╗    ██╗ ██████╗ ██╗    ██╗███████╗███████╗██████╗\n"
+    header_art += "\t██║    ██║██╔═══██╗██║    ██║╚══███╔╝██╔════╝██╔══██╗\n"
+    header_art += "\t██║ █╗ ██║██║   ██║██║ █╗ ██║  ███╔╝ █████╗  ██████╔╝\n"
+    header_art += "\t██║███╗██║██║   ██║██║███╗██║ ███╔╝  ██╔══╝  ██╔══██╗\n"
+    header_art += "\t╚███╔███╔╝╚██████╔╝╚███╔███╔╝███████╗███████╗██║  ██║\n"
+    header_art += "\t╚══╝╚══╝  ╚═════╝  ╚══╝╚══╝ ╚══════╝╚══════╝╚═╝  ╚═╝\n"
     header = "\n******************************************************************************************\n"
     header += "* " + APP_NAME + " version " + APP_VERSION + " <" + WEBSITE_LINK + ">.\n"
-    header += "* Copyright (C) " + COPRIGHT_YEAR + " " + AUTHOR + "\n"
+    header += "* Copyright (C) " + COPYRIGHT_YEAR + " " + AUTHOR + "\n"
     header += "*\n"
     header += "* " + APP_NAME + " is free software: you can redistribute it and/or modify it under the terms of the\n"
     header += "* GNU General Public License as published by the Free Software Foundation, either version 3\n"
@@ -33,3 +33,7 @@ if __name__ == "__main__":
     header += "******************************************************************************************\n"
     print(header_art, header)
     main.run()
+
+
+if __name__ == "__main__":
+    run()
