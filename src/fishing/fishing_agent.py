17c83dfbe9c92b6518e20826861064c0a0757025
Frank Pereny <fjpereny@gmai.com>
2022-12-04 00:47:47 +0800

    Fixed fishing and improved FPS refresh rate.

--- src/fishing/fishing_agent.py
+++ src/fishing/fishing_agent.py
@@ -1,73 +1,53 @@
 
-import cv2
+import cv2 as cv
 import numpy as np
 import pyautogui
 import time
 from threading import Thread
+import os
 
 
 class FishingAgent:
     def __init__(self, main_agent):
         self.main_agent = main_agent
-        self.fishing_target = cv2.imread("fishing/assets/fishing_target.png")
-        self.lure_location = None
-        self.lure_pixel_box = None
-        self.fishing = False
-        self.cast_time = None
+        self.fishing_target = cv.imread("/home/frank/Git/wowzer/src/fishing/assets/fishing_target.png")
 
     def cast_lure(self):
         print("Casting!...")
         self.fishing = True
         self.cast_time = time.time()
         pyautogui.press('1')
+        time.sleep(3)
         self.find_lure()
 
     def find_lure(self):
         start_time = time.time()
-        while self.lure_location is None:
-            self.lure_location = pyautogui.locateOnScreen(self.fishing_target, confidence=0.8)
-            if time.time() - start_time > 15:
-                print("Warning: Failed to find lure")
-                break
-        if self.lure_location:
-            lure_pixel_box_x0 = self.lure_location[0] - 50
-            lure_pixel_box_x1 = self.lure_location[0] + 50
-            lure_pixel_box_y0 = self.lure_location[1] - 50
-            lure_pixel_box_y1 = self.lure_location[1] + 50
-            self.lure_pixel_box = [lure_pixel_box_x0, lure_pixel_box_y0, lure_pixel_box_x1, lure_pixel_box_y1]
-            self.move_to_lure()
+        lure_location = cv.matchTemplate(self.main_agent.cur_img, self.fishing_target, cv.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(lure_location)
+        self.lure_location = max_loc
+        self.move_to_lure()
 
     def move_to_lure(self):
         if self.lure_location:
-            pyautogui.moveTo(self.lure_location[0] + 20, self.lure_location[1] + 10, .45, pyautogui.easeOutQuad)
+            pyautogui.moveTo(self.lure_location[0] + 25, self.lure_location[1], .45, pyautogui.easeOutQuad)
             self.watch_lure()
         else:
             print("Warning: Attempted to move to lure_location, but lure_location is None (fishing_agent.py line 32)")
             return False
 
     def watch_lure(self):
-        start_time = time.time()
-        tol = 0.05    # Percent tolerance
-        avg_col = np.average(self.main_agent.cur_img.crop(self.lure_pixel_box))
-        cur_avg_col = avg_col
-        while (1 - tol) * avg_col < cur_avg_col < (1 + tol) * avg_col:
-            cur_avg_col = np.average(self.main_agent.cur_img.crop(self.lure_pixel_box))
-            if time.time() - start_time >= 30:
+        time_start = time.time()
+        while True:
+            pixel = self.main_agent.cur_imgHSV[self.lure_location[1] + 25][self.lure_location[0]]
+            if pixel[0] >= 20 or pixel[1] < 60 or pixel[2] < 40 or time.time() - time_start >= 30:
+                print("Bite detected!")
                 break
-        # self.pull_line()
+        self.pull_line()
 
     def pull_line(self):
-        pyautogui.keyDown('shift')
-        pyautogui.click(button='right')
-        pyautogui.keyUp('shift')
+        os.system("sh -c 'xdotool keydown Shift_L; sleep 0.1; xdotool mousedown 3; sleep 0.1; xdotool mouseup 3; sleep 0.1; xdotool keyup Shift_L; sleep 0.1'")
+        time.sleep(3)
 
     def run(self):
         while True:
-            if self.cast_time is not None and (time.time() - self.cast_time) >= 35:
-                self.lure_location = None
-                self.lure_pixel_box = None
-                self.fishing = False
-                self.cast_time = None
-
-            if self.fishing is False:
-                self.cast_lure()
+            self.cast_lure()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-1
-from PIL import Image
+
+from PIL import Image, ImageGrab
 import time
 from threading import Thread
 import wowzer
 from Xlib import display, X
+import numpy as np
+import cv2 as cv
 
 import sight.mana_bar as mana_bar
 import fishing.fishing_agent as fishing_agent
@@ -12,6 +14,7 @@ import fishing.fishing_agent as fishing_agent
 class MainAgent:
     def __init__(self):
         self.cur_img = None
+        self.cur_imgHSV = None
 
 
 def update_screen(agent):
@@ -22,11 +25,24 @@ def update_screen(agent):
     width = screen.width_in_pixels
     height = screen.height_in_pixels
     print("Detected display resolution: " + str(width) + " x " + str(height))
+
+    loop_time = time.time()
+    show_fps = False
+
     while True:
-        raw = root.get_image(0, 0, width, height, X.ZPixmap, 0xffffff)
-        agent.cur_img = Image.frombytes("RGB", (width, height), raw.data, "raw", "BGRX")
-        print("update")
-        time.sleep(0.01)
+        screenshot = ImageGrab.grab()
+        screenshot = np.array(screenshot)
+        screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+        screenshotHSV = cv.cvtColor(screenshot, cv.COLOR_BGR2HSV)
+        agent.cur_img = screenshot
+        agent.cur_imgHSV = screenshotHSV
+
+        # cv_scale = 0.35
+        # comp_vision = cv.resize(screenshot, (0, 0), fx=cv_scale, fy=cv_scale)
+        # cv.imshow("Computer Vision", comp_vision)
+        # print('FPS: {}'.format(1 / (time.time() - loop_time)))
+        loop_time = time.time()
+        cv.waitKey(1)
 
 
 def run():
@@ -34,8 +50,8 @@ def run():
     update_screen_thread = Thread(target=update_screen, args=(main_agent,), name="update screen thread")
     update_screen_thread.start()
 
-    mana_bar_thread = Thread(target=mana_bar.watch_mana, args=(main_agent,), name="mana bar")
-    mana_bar_thread.start()
+    # mana_bar_thread = Thread(target=mana_bar.watch_mana, args=(main_agent,), name="mana bar")
+    # mana_bar_thread.start()
 
     fishing_agent_thread = fishing_agent.FishingAgent(main_agent)
     time.sleep(5)
